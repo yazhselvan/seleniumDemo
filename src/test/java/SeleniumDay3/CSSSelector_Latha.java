03e0358616f3f35d7727f5a15efa807b233bbd35
latha <lathac0707@gmail.com>
2026-05-31 20:23:02 +0530

    Cssselector changes done

--- src/test/java/SeleniumDay3/CSSSelector_Latha.java
+++ src/test/java/SeleniumDay3/CSSSelector_Latha.java
@@ -1,5 +1,6 @@
 package SeleniumDay3;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,13 +11,15 @@ public class CSSSelector_Latha {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.name("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-        driver.findElement(By.linkText("Sauce Labs Backpack")).click();
+        driver.findElement(By.cssSelector("input#user-name")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("input[name='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("input.submit-button")).click();
+        driver.findElement(By.xpath("//span[text()='OK']")).click();
+        driver.findElement(By.cssSelector("div[data-test='inventory-item-name']")).click();
         driver.findElement(By.cssSelector("button#add-to-cart")).click();
         driver.findElement(By.cssSelector("button#checkout")).click();
         driver.quit();
 
     }
 }
+
